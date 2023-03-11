9f219e35e1c860db9b78cfc27e5437f798ae2ba8
wangxingyu@mucang.cn <wangxingyu@mucang.cn>
2023-03-11 13:43:42 +0800

    12

--- src/config/website.js
+++ src/config/website.js
@@ -8,9 +8,9 @@ export default {
   indexTitle: 'Saber Admin',
   clientId: 'saber', // 客户端id
   clientSecret: 'saber_secret', // 客户端密钥
-  tenantMode: true, // 是否开启租户模式
+  tenantMode: false, // 是否开启租户模式
   tenantId: "000000", // 管理组租户编号
-  captchaMode: true, // 是否开启验证码模式
+  captchaMode: false, // 是否开启验证码模式
   switchMode: false, // 是否开启部门切换模式
   lockPage: '/lock',
   tokenTime: 3000,
